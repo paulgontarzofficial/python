81581f6c3c5fb42c304873a7baef1b3a53aed884
paulgontarzofficial <paulgontarz70@gmail.com>
2025-05-14 06:32:43 -0700

    Added new exercises and preactice python files.

---
+++ practice/formatstrings.py
@@ -0,0 +1,6 @@
+# def calc_string(first_number, second_number):
+#    return print("The sum of", first_number, "and", second_number, "is", first_number + second_number,".") 
+
+
+def calc_string(first_number, second_number):
+    return print(f"The sum of {first_number} and {second_number} is {first_number + second_number}.")
---
+++ practice/replacestring.py
@@ -0,0 +1,4 @@
+def replace_string(orig_string, old_string, new_string):
+     return orig_string.replace(old_string, new_string)
+
+replace_string()
